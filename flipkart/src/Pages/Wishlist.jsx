333877d1c55142b9a19631b1499705060146af95
POOVARASAN-35 <arasu5070go@gmail.com>
2026-03-06 17:01:00 +0530

    Fix CSS case issue for Vercel build

--- flipkart/src/Pages/Wishlist.jsx
+++ flipkart/src/Pages/Wishlist.jsx
@@ -1,7 +1,7 @@
 import { useCart } from "../context/CartContext";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/wishlist.css";
+import "../styles/Wishlist.css";
 
 const Wishlist = () => {
   const { wishlist, cart, removeFromWishlist, moveToCart } = useCart();
