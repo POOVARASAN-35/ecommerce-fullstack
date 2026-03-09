cfd118fb4ab421c6db1839e530181a76695a577f
POOVARASAN-35 <arasu5070go@gmail.com>
2026-03-09 11:07:38 +0530

    wishlist route fix

--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/wishlistController");
-
+router.get("/", (req, res) => {
+  res.json({
+    message: "Wishlist API is working",
+    endpoint: "/api/wishlist/:userId"
+  });
+});
 router.post("/add", controller.addToWishlist);
 router.get("/:userId", controller.getWishlist);
 router.delete("/:id", controller.removeFromWishlist);
