d24db57dbbf76439b4b4e293fe6d32c52d4a942a
POOVARASAN-35 <arasu5070go@gmail.com>
2026-03-12 14:47:10 +0530

    fix email smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,19 +2,23 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     }
   });
 
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: `"ShopKart" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html
   });
+
+  console.log("Email sent:", info.response);
 };
 
 module.exports = sendEmail;
